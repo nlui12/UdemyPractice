1fd8d32c11530b90e9cceb8c93fdaa21015130b6
Nick <nicklui1998@gmail.com>
2022-08-01 18:14:40 -0700

    question 1 done

---
+++ Python/assessment7.py
@@ -0,0 +1,11 @@
+# Python Udemy Assesment for 07-statements
+
+#question 1: Use for, .split(), and if to create a Statement that will print out words that start with 's'
+
+st = 'Print only the words that start with s in this sentence'
+
+#the split() method takes a string and depending on the delimiter splits the words into elements of a list
+for letter in st:
+    print(letter)
+
+
